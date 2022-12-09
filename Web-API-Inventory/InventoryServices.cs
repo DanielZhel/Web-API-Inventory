edb04555de94afd3d53788f302cdc32ad7935d93
Danielzhel <fartunazh@gmail.com>
2022-12-09 14:56:30 +0300

    Добавьте файлы проекта.

---
+++ Web-API-Inventory/InventoryServices.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace Web_API_Inventory
+{
+    public class InventoryServices
+    {
+        public List<InventorySettings> prods = new List<InventorySettings>();
+        public InventoryServices(string jsonPath)
+        {
+            string jsonString = File.ReadAllText(jsonPath);
+            prods = JsonSerializer.Deserialize<List<InventorySettings>>(jsonString);
+
+        }
+    }
+}
